523b983709f19a9b307d0aad493718c93788fdea
hwaninging <hwanhee@sookmyung.ac.kr>
2025-09-25 15:40:40 +0900

    실습2
    사칙연산 계산기

--- main.c
+++ main.c
@@ -3,21 +3,17 @@
 
 int main(int argc, char *argv[])
 {
-    int x, y, z;
-    float m;
-    int a, b, c;
-    x = 2;
-    z = 1;
-    a = 3;
-    b = 4;
-    c = 5;
+    int x, y;
     
-    y = a*x*x + b*x + c;
+    printf("Input two integers :"); 
+    scanf("%i %i", &x, &y);
     
-    m = (x+y+z)/3;
+    printf("+ result is : %i\n", x+y);
+    printf("- result is : %i\n", x-y);
+    printf("* result is : %i\n", x*y);
+    printf("/ result is : %i\n", x/y);
+    printf("%% result is : %i\n", x%y);
     
-    printf("y=%d, m=%f\n", y,m);
-     
     system("PAUSE");	
     return 0;
 }
