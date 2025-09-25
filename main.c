2ce7e55fc137408a9646207301a9bad62d9eee91
hwaninging <hwanhee@sookmyung.ac.kr>
2025-09-25 16:15:03 +0900

    실습5
    1의 개수 계산

--- main.c
+++ main.c
@@ -3,13 +3,21 @@
 
 int main(int argc, char *argv[])
 {
-    int year; //윤년 계산  
+    unsigned int x;
+    int b;
     
-    printf("Input the year :");  
-    scanf("%i", &year);  
+    printf("Input the number : ");  
+    scanf("%ui", &x);  
     
-    printf("Is the year %i the leap year? %i\n", year, (year%4==0 && year%100!=0)||(year%400==0));
-    //400으로 나누어 떨어지면 윤년, != : 아니라는 뜻 
+    for (b=0; x != 0; x >>= 1)
+    {
+        if (x & 1)
+        {
+              b++;
+        }
+    }
+    
+    printf("The result is : %i\n", b); 
     
     system("PAUSE");	
     return 0;
